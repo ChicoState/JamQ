1805d0d86a9b9c6f8e516584fa8e8729bc427a24
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-09-27 20:29:52 -0700

    rehauled search page, leveraging search api + documentation

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -15,39 +15,53 @@ import { OAuth } from 'oauthio-web';
   templateUrl: 'search.html',
 })
 export class SearchPage {
-  user: any;
-  searchQuery: string = '';
-  items: string[];
+  spotifyApi: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+        //getting spotify api library
+    var SpotifyWebApi = require('spotify-web-api-node');
+    //build api with no params
+    this.spotifyApi = new SpotifyWebApi();
+    //gets auth from cache named 'spotify'
+    var spotify = OAuth.create('spotify');
+    //sets access token of authenticated user
+    this.spotifyApi.setAccessToken(spotify.access_token);
 
-    this.initializeItems();
+
   }
-  initializeItems() {
-    this.items = [
-      'Artist',
-      'Song',
-      'Backstreet Boyz',
-      'Homeless guy with drumsticks'
-    ];
-  }
+
   getItems(ev: any) {
-    // Reset items back to all of the items
-    this.initializeItems();
 
     // set val to the value of the searchbar
-    let val = ev.target.value;
-
+    let queryTerm = ev.target.value;
     // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
+    if (queryTerm && queryTerm.trim() != '') {
+    //search track titles and return top 10 results
+    var prev = this.spotifyApi.searchTracks(queryTerm, {limit: 10})
+        .then(function(data) {
+          //song object for easier calls
+          let song = data.body.tracks.items['0'];
+
+          // clean the promise so it doesn't call abort
+          prev = null;
+
+          //pushing elements to page
+          (<HTMLImageElement>document.getElementById("img")).src = song.album.images['0'].url;
+          document.getElementById("title").innerHTML = song.name;
+          document.getElementById("artist").innerHTML = song.artists['0'].name;
+
+
+        }, function(err) { //some error checking
+          console.error(err);
+        });
     }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
+  }
+  
+  itemTapped(event, item) {
   }
 
 }
