6d42661096afe5adc03ca11eedc6df1367044cfb
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-10-25 16:57:13 -0700

    add party key and party owner to list view

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -11,19 +11,27 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class ListPage {
   songs: FirebaseListObservable<any>;
+  owner: FirebaseObjectObservable<any>;
+  host: any;
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string,id: number}>;
   key: any;
+  partyKey: any;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public af: AngularFireDatabase) {
     //get key returned from join party page
     this.key = this.navParams.get('hostKey');
     //use key to access correct queue
 
-    var partyKey = sessionStorage['partyCookie'];
-    console.log(partyKey);
-    this.songs = af.list("/" + partyKey + "/songlist");
+    this.partyKey = sessionStorage['partyCookie'];
+    console.log(this.partyKey);
+    this.songs = af.list("/" + this.partyKey + "/songlist");
+    this.owner = af.object("/"+ this.partyKey);
+    this.owner.subscribe(snapshot => this.host = snapshot.owner);
+    // console.log(this.owner);
+
     //var firebaseKey = '/333/songlist'; //will get rid of this once its working
   }
 
