d61f5898a079035d9a10c8c79b127dbb0c017e38
logm <logm>
2017-10-26 11:20:17 -0700

    created functions for like/dislike --- not working yet just placeholders

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -6,8 +6,8 @@ import { AngularFireAuth } from 'angularfire2/auth';
 
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+  selector: "page-list",
+  templateUrl: "list.html"
 })
 export class ListPage {
   songs: FirebaseListObservable<any>;
@@ -15,24 +15,48 @@ export class ListPage {
   host: any;
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string,id: number}>;
+  items: Array<{ title: string; note: string; icon: string; id: number }>;
   key: any;
   partyKey: any;
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,public af: AngularFireDatabase) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public af: AngularFireDatabase
+  ) {
     //get key returned from join party page
-    this.key = this.navParams.get('hostKey');
+    this.key = this.navParams.get("hostKey");
     //use key to access correct queue
 
-    this.partyKey = sessionStorage['partyCookie'];
+    this.partyKey = sessionStorage["partyCookie"];
     console.log(this.partyKey);
     this.songs = af.list("/" + this.partyKey + "/songlist");
-    this.owner = af.object("/"+ this.partyKey);
-    this.owner.subscribe(snapshot => this.host = snapshot.owner);
+    this.owner = af.object("/" + this.partyKey);
+    this.owner.subscribe(snapshot => (this.host = snapshot.owner));
     // console.log(this.owner);
 
     //var firebaseKey = '/333/songlist'; //will get rid of this once its working
   }
 
+  like(songid) {
+    this.songs.forEach(song => {
+      song.forEach(song => {
+        if (song.songid == songid) {
+          //this.songs.remove(song.$key);
+          console.log("like");
+        }
+      });
+    });
+  }
+
+  dislike(songid) {
+    this.songs.forEach(song => {
+      song.forEach(song => {
+        if (song.songid == songid) {
+          //this.songs.remove(song.$key);
+          console.log("dislike");
+        }
+      });
+    });
+  }
 }
