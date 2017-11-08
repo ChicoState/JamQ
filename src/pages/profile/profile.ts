9686fd6e4a3d412d19fcba8b7df20a24d86f0b4b
logm <logm>
2017-11-07 20:43:54 -0800

    started work on merging home page to profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { OAuth as OAuthWeb } from 'oauthio-web';
-import { OAuth } from 'oauth-phonegap';
-import { Platform } from 'ionic-angular';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { AngularFireAuth } from "angularfire2/auth";
+import { OAuth as OAuthWeb } from "oauthio-web";
+import { OAuth } from "oauth-phonegap";
+import { Platform } from "ionic-angular";
+import {
+  AngularFireDatabase,
+  FirebaseListObservable
+} from "angularfire2/database";
 
 /**
  * Generated class for the ProfilePage page.
@@ -16,8 +18,8 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: "page-profile",
+  templateUrl: "profile.html"
 })
 export class ProfilePage {
   spotify: any;
@@ -25,14 +27,20 @@ export class ProfilePage {
   isMobile: any;
 
   constructor(
-    private afAuth: AngularFireAuth, private platform: Platform,
-    public navCtrl: NavController, public navParams: NavParams) {
-      //checks if device is mobile or Web
-    if (platform.is('cordova')) { this.isMobile = true; }
-    else { this.isMobile = false; }
+    private afAuth: AngularFireAuth,
+    private platform: Platform,
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {
+    //checks if device is mobile or Web
+    if (platform.is("cordova")) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
 
     //getting spotify api library
-    var SpotifyWebApi = require('spotify-web-api-node');
+    var SpotifyWebApi = require("spotify-web-api-node");
     //build api with no params
     this.spotifyApi = new SpotifyWebApi();
 
@@ -40,11 +48,11 @@ export class ProfilePage {
     if (this.isMobile) {
       //is phone
       //gets auth from cache named 'spotify'
-      this.spotify = OAuth.create('spotify');
+      this.spotify = OAuth.create("spotify");
     } else {
       //is web
       //gets auth from cache named 'spotify'
-      this.spotify = OAuthWeb.create('spotify');
+      this.spotify = OAuthWeb.create("spotify");
       console.log("is Web");
     }
     //sets access token of authenticated user
@@ -55,78 +63,163 @@ export class ProfilePage {
     if (this.isMobile == true) {
       //is phone
       this.mobileAuth();
-      let spotify = document.getElementById('spotify');
-      let spotifyfull = document.getElementById('spotifyfull');
+      let spotify = document.getElementById("spotify");
+      let spotifyfull = document.getElementById("spotifyfull");
       spotify.style.visibility = "visible";
       spotify.innerHTML = "Spotify Signed In";
       let page = document.getElementById("page");
       spotifyfull.style.visibility = "hidden";
-      page.replaceChild(spotify,spotifyfull);
+      page.replaceChild(spotify, spotifyfull);
     } else {
       //is web
       this.webAuth();
-      let spotify = document.getElementById('spotify');
-      let spotifyfull = document.getElementById('spotifyfull');
+      let spotify = document.getElementById("spotify");
+      let spotifyfull = document.getElementById("spotifyfull");
       spotify.style.visibility = "visible";
       spotify.innerHTML = "Spotify Signed In";
       let page = document.getElementById("page");
       spotifyfull.style.visibility = "hidden";
-      page.replaceChild(spotify,spotifyfull);
+      page.replaceChild(spotify, spotifyfull);
     }
   }
-
 
   ionViewDidLoad() {
     if (this.spotify.access_token) {
-      let spotify = document.getElementById('spotify');
-      let spotifyfull = document.getElementById('spotifyfull');
+      let spotify = document.getElementById("spotify");
+      let spotifyfull = document.getElementById("spotifyfull");
       spotify.style.visibility = "visible";
       spotify.innerHTML = "Spotify Signed In";
       let page = document.getElementById("page");
       spotifyfull.style.visibility = "hidden";
-      page.replaceChild(spotify,spotifyfull);
+      page.replaceChild(spotify, spotifyfull);
     }
-    console.log('ionViewDidLoad ProfilePage');
+    console.log("ionViewDidLoad ProfilePage");
   }
 
   //use for authentiating with mobile libraries
-mobileAuth(){
-  //initializes spotify auth
-  OAuth.initialize('NJG7cpjPQHkVhSQgvpQi5MRoyM4');
-  //popup for spotify login
-  //then resets html to wait for auth to complete
-    //idealy this would be a loading feature to wait until popup closes with success
-  //on error sends alert  to page for debbuging
-  let spotifyApi = this.spotifyApi;
-    OAuth.popup('spotify',{cache: true}).done(function(spotify) {
-      spotifyApi.setAccessToken(spotify.access_token);
-    }).then(     ).fail(function(err) {
-      alert("Error with spotify login");
-    });
-
-}
-//use for authenticating with web libraries
-webAuth() {
-  var spotify = OAuthWeb.create('spotify');
-  if(spotify.access_token){
-    // console.log(spotify.access_token);
-
-  } else {
-  OAuthWeb.initialize('NJG7cpjPQHkVhSQgvpQi5MRoyM4');
+  mobileAuth() {
+    //initializes spotify auth
+    OAuth.initialize("NJG7cpjPQHkVhSQgvpQi5MRoyM4");
     //popup for spotify login
     //then resets html to wait for auth to complete
-      //idealy this would be a loading feature to wait until popup closes with success
+    //idealy this would be a loading feature to wait until popup closes with success
     //on error sends alert  to page for debbuging
     let spotifyApi = this.spotifyApi;
-      OAuthWeb.popup('spotify',{cache: true}).done(function(spotify) {
+    OAuth.popup("spotify", { cache: true })
+      .done(function(spotify) {
         spotifyApi.setAccessToken(spotify.access_token);
-      }).then(       ).fail(function(err) {
+      })
+      .then()
+      .fail(function(err) {
         alert("Error with spotify login");
       });
+  }
+  //use for authenticating with web libraries
+  webAuth() {
+    var spotify = OAuthWeb.create("spotify");
+    if (spotify.access_token) {
+      // console.log(spotify.access_token);
+    } else {
+      OAuthWeb.initialize("NJG7cpjPQHkVhSQgvpQi5MRoyM4");
+      //popup for spotify login
+      //then resets html to wait for auth to complete
+      //idealy this would be a loading feature to wait until popup closes with success
+      //on error sends alert  to page for debbuging
+      let spotifyApi = this.spotifyApi;
+      OAuthWeb.popup("spotify", { cache: true })
+        .done(function(spotify) {
+          spotifyApi.setAccessToken(spotify.access_token);
+        })
+        .then()
+        .fail(function(err) {
+          alert("Error with spotify login");
+        });
+    }
+  }
+  goQueue() {}
+  newParty() {}
+  goParty() {}
+
+  /*
+  goQueue() {
+    // this.partyKey = document.getElementById('party').innerHTML
+    //create obj for passing key to next page
+    var data = { hostKey: this.partyKey };
+    //var uniquePartyKey = data.toString();
+    var uniquePartyKey = parseInt(this.partyKey);
+    //console.log(uniquePartyKey);
+    if (isNaN(uniquePartyKey)) {
+      alert("Please enter a party number");
+      return;
+    } else if (uniquePartyKey < 1000 || uniquePartyKey > 9999) {
+      // later we should check if the party already exists in the db
+      alert("Party number does not exist");
+      return;
     }
 
-}
+    sessionStorage["partyCookie"] = this.partyKey;
+    sessionStorage["role"] = "guest"; //maybe later have it check if its your party or not
 
+    //make sure that the key exists in the DB
+    //if key is not in DB display alert and then go to home page again
+    //Then enter party
 
+    //remove user menu pptions
+    this.menuCtrl.enable(true, "user");
+    this.menuCtrl.enable(false, "host");
 
+    //takes user to queue with data containing party key
+    this.navCtrl.setRoot(ListPage, data);
+    //this.navCtrl.setRoot(NowplayingPage);
+  }
+  //navigates to and sets root to host now playing page
+  newParty() {
+    //later we can check this and make sure that there is not already a party with that number
+    //later we can check if the user is already hosting a party
+    var randomServerNum = Math.floor(1000 + Math.random() * 9000);
+    this.partyKey = randomServerNum.toString();
+
+    sessionStorage["partyCookie"] = this.partyKey;
+    sessionStorage["role"] = "host"; //maybe later have it check if its your party or not
+
+    //create the db observable to manipulate
+    this.party = this.af.object("/" + this.partyKey);
+    let db = this.party;
+    this.afAuth.authState.subscribe(data => {
+      db.set({
+        owner: data.email
+      });
+    });
+
+    //create new table in db with corresponding key
+
+    //eable host menu/disable user
+    this.menuCtrl.enable(false, "user");
+    this.menuCtrl.enable(true, "host");
+
+    this.navCtrl.setRoot(NowplayingPage);
+  }
+
+  goParty() {
+    //create obj for passing key to next page
+    var data = { hostKey: this.partyKey };
+    //var uniquePartyKey = data.toString();
+    var uniquePartyKey = parseInt(this.partyKey);
+    //console.log(uniquePartyKey);
+    if (isNaN(uniquePartyKey)) {
+      alert("Please enter a party number");
+      return;
+    } else if (uniquePartyKey < 1000 || uniquePartyKey > 9999) {
+      // later we should check if the party already exists in the db
+      alert("Party number does not exist");
+      return;
+    }
+
+    sessionStorage["partyCookie"] = this.partyKey;
+    sessionStorage["role"] = "host"; //maybe later have it check if its your party or not
+    this.navCtrl.setRoot(NowplayingPage);
+    this.menuCtrl.enable(false, "user");
+    this.menuCtrl.enable(true, "host");
+  }
+  */
 }
