f959e453d5ccea19119a76e78b80da6298d58c6b
german9304 <31298672+german9304@users.noreply.github.com>
2017-11-07 23:53:41 -0800

    Fixed queue duplicate error

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -222,16 +222,36 @@ export class SearchPage {
             //shorten call
             let track = data.body;
             //send track information to firebase
-            db.push({
-              artist: track.artists['0'].name,
-              title: track.name,
-              songid: id,
-              img: track.album.images['0'].url,
+         var inqueue = true;
+         var index;
+         var key;
+         var song_likes;
+       //if song is in the queue, add one like
+       //otherwise add to queue
+        db.subscribe(songs =>{
+         for( index=0;index<songs.length;index++){ //for loop to search for songid
+           if(songs[index].songid==id){
+             key= songs[index].$key;
+             song_likes= songs[index].likes;
+             inqueue=false;
+              break;
+           }
+          }
+         })
+         if(inqueue==true){
+          db.push({
+            artist: track.artists['0'].name,
+            title: track.name,
+            songid: id,
+            img: track.album.images['0'].url,
 
-              //maybe make a new table with likes columns of users who liked it and new table of users who disliked it
-              likes: 1,// change to spotify users
-              //dislikes: 0 // change to spotify users
-            });
+            //maybe make a new table with likes columns of users who liked it and new table of users who disliked it
+            likes: 1,// change to spotify users
+            //dislikes: 0 // change to spotify users
+          });
+        }else{
+        db.update(key, { likes: song_likes + 1 }); //likes update
+        }
 
         }, function(err) { //error checking
         console.log('Something went wrong!', err);
