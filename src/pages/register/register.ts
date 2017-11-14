0667913e6b039cd8f58ed4c6bc1a724d57bc299d
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-11-14 15:51:21 -0800

    added password validation and username fields

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
-import { AngularFireAuth } from 'angularfire2/auth'
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase} from 'angularfire2/database';
+import 'rxjs/add/operator/take';
 
 /**
  * Generated class for the RegisterPage page.
@@ -19,7 +21,7 @@ export class RegisterPage {
   user = {} as User;
 
   constructor(
-    private afAuth: AngularFireAuth,
+    private afAuth: AngularFireAuth, private afDatabase: AngularFireDatabase,
     public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -28,13 +30,23 @@ export class RegisterPage {
   }
 
   async register(user: User,) {
-    try {
-      const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email,user.password);
-      console.log(result);
-    } catch (error) {
-      alert(error)
-      console.log(error)
+    if(user.password != user.verify) {
+      alert("Passwords don't match")
+    } else {
+      try {
+        const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email,user.password);
+        // console.log(result);
+        if(result) {
+          this.afAuth.authState.take(1).subscribe(auth => {
+            this.afDatabase.list(`users/${auth.uid}`).set("username", this.user.username);
+          })
+        }
+      } catch (error) {
+        alert(error)
+        // console.log(error)
+      }
     }
-  }
+
+    }
 
 }
