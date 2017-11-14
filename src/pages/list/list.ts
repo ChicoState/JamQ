428b20726c6ebb2c623508e48c3798510ae4e1c1
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-11-14 15:45:14 -0800

    implimented one like per user feature

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -22,12 +22,17 @@ import "rxjs/add/operator/map";
 export class ListPage {
   songs: FirebaseListObservable<any>;
   owner: FirebaseObjectObservable<any>;
+  user_likes: FirebaseListObservable<any>;
+  user_dislikes: FirebaseListObservable<any>;
+  likeCheck: FirebaseListObservable<any>;
+  dislikeCount: FirebaseObjectObservable<any>;
   host: any;
   selectedItem: any;
   icons: string[];
   items: Array<{ title: string; note: string; icon: string; id: number }>;
   key: any;
   partyKey: any;
+  username: any;
 
   constructor(
     private afAuth: AngularFireAuth,
@@ -35,11 +40,20 @@ export class ListPage {
     public navParams: NavParams,
     public af: AngularFireDatabase
   ) {
+    //use key to access correct queue
+    this.partyKey = sessionStorage["partyCookie"];
+
+    this.afAuth.authState.subscribe(auth => {
+    this.af.object(`users/${auth.uid}`).take(1).subscribe(data => {
+      this.username = data.username;
+      this.user_likes = af.list("/" + this.partyKey + "/userlist/" + data.username + "/likes");
+      this.user_dislikes = af.list("/" + this.partyKey + "/userlist/" + data.username + "/dislikes");
+    });
+  });
+
+
     //get key returned from join party page
     this.key = this.navParams.get("hostKey");
-    //use key to access correct queue
-
-    this.partyKey = sessionStorage["partyCookie"];
     //console.log(this.partyKey);
     this.songs = af.list("/" + this.partyKey + "/songlist");
     this.owner = af.object("/" + this.partyKey);
@@ -57,10 +71,46 @@ export class ListPage {
   This function only adds up to 20 likes
   *****/
   like(song) {
-    this.songs.update(song.$key, { likes: song.likes + 1 });
-    //sleep(5);
+    var temp = [];
+    this.likeCheck = this.af.list("/" + this.partyKey + "/userlist/" + this.username + "/likes");
+    this.likeCheck.subscribe(data => {
+      // console.log(data)
+        data.forEach(item => {
+            // console.log(item.song)
+            temp.push(item.song)
+        })
+        // if (exists == -1 || exists == 0) {
+        //   this.user_likes.push({song: song.$key});
+        //   this.songs.update(song.$key, { likes: song.likes + 1 });
+        // }
+      // } else if (exists == 0) {
+      //     console.log("gettinghere")
+      //       this.user_likes.push({song: song.$key});
+      //       this.songs.update(song.$key, { likes: song.likes + 1 });
+      // }
+
+    })
+    console.log(temp)
+    var check = false;
+    if(temp.length == 0) {
+      this.user_likes.push({song: song.$key});
+      this.songs.update(song.$key, { likes: song.likes + 1 });
+    } else {
+      for(var i = 0; i<temp.length; i++) {
+        console.log("checking songs")
+        if(temp[i] == song.$key) {
+          check = true;
+          break;
+        }
+      }
   }
+  if(!check) {
+      this.user_likes.push({song: song.$key});
+      this.songs.update(song.$key, { likes: song.likes + 1 });
+  }
+}
   dislike(song) {
+    this.user_dislikes.push({song: song.$key});
     this.songs.update(song.$key, { likes: song.likes - 1 });
     //sleep(5);
   }
