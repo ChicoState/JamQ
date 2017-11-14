d1414e63528079e5e3ac2ac41f82137fef20d4a3
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-11-14 15:43:58 -0800

    added user model including username

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,11 @@
-import { FirebaseListObservable } from 'angularfire2/database';
+// import { FirebaseListObservable } from 'angularfire2/database';
 
 export interface User {
+
   email: string;
   password: string;
+  verify: string;
   username: string;
-  partiesHosted: FirebaseListObservable<any>;
-  partiesJoined: FirebaseListObservable<any>;
+  // partiesHosted: FirebaseListObservable<any>;
+  // partiesJoined: FirebaseListObservable<any>;
 }
