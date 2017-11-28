7c5ff7eabcfa51596512155a06e256384e537c76
logm <loganmarkle@gmail.com>
2017-11-28 10:37:10 -0800

    refactored

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -14,6 +14,7 @@ import { SearchPage } from "../search/search";
 import { SlidesPage } from "../slides/slides";
 import { ProfilePage } from "../profile/profile";
 import "rxjs/add/operator/map";
+import { OAuth as OAuthWeb } from 'oauthio-web';
 
 @Component({
   selector: "page-list",
@@ -44,18 +45,18 @@ export class ListPage {
     this.partyKey = sessionStorage["partyCookie"];
 
     this.afAuth.authState.subscribe(auth => {
-    this.af.object(`users/${auth.uid}`).take(1).subscribe(data => {
-      this.username = data.username;
-      this.user_likes = af.list("/" + this.partyKey + "/userlist/" + data.username + "/likes");
-      this.user_dislikes = af.list("/" + this.partyKey + "/userlist/" + data.username + "/dislikes");
+      this.af.object(`users/${auth.uid}`).take(1).subscribe(data => {
+        this.username = data.username;
+        this.user_likes = af.list("/" + this.partyKey + "/userlist/" + data.username + "/likes");
+        this.user_dislikes = af.list("/" + this.partyKey + "/userlist/" + data.username + "/dislikes");
+      });
     });
-  });
 
 
     //get key returned from join party page
     //this.key = this.navParams.get("hostKey");
     this.partyKey = sessionStorage["partyCookie"];
-    
+
     //console.log(this.partyKey);
     this.songs = af.list("/" + this.partyKey + "/songlist");
     this.owner = af.object("/" + this.partyKey);
@@ -72,18 +73,18 @@ export class ListPage {
 
   like(song) {
     var temp = [];
-     var check = false;
+    var check = false;
     this.likeCheck = this.af.list("/" + this.partyKey + "/userlist/" + this.username + "/likes");
     this.likeCheck.subscribe(data => {
-        data.forEach(item => {
-            // console.log(item.song)
-            temp.push(item.song)
-        })
+      data.forEach(item => {
+        // console.log(item.song)
+        temp.push(item.song)
+      })
 
-        // if (exists == -1 || exists == 0) {
-        //   this.user_likes.push({song: song.$key});
-        //   this.songs.update(song.$key, { likes: song.likes + 1 });
-        // }
+      // if (exists == -1 || exists == 0) {
+      //   this.user_likes.push({song: song.$key});
+      //   this.songs.update(song.$key, { likes: song.likes + 1 });
+      // }
       // } else if (exists == 0) {
       //     console.log("gettinghere")
       //       this.user_likes.push({song: song.$key});
@@ -93,34 +94,29 @@ export class ListPage {
     })
     //console.log(temp)
     var check = false;
-    if(temp.length == 0) {
-      this.user_likes.push({song: song.$key});
+    if (temp.length == 0) {
+      this.user_likes.push({ song: song.$key });
       this.songs.update(song.$key, { likes: song.likes + 1 });
-      check=true;
+      check = true;
     } else {
-      for(var i = 0; i<temp.length; i++) {
+      for (var i = 0; i < temp.length; i++) {
         console.log("checking songs")
-        if(temp[i] == song.$key) {
+        if (temp[i] == song.$key) {
           check = true;
           break;
         }
       }
-  }
+    }
 
 
-  if(check==false) {
-      this.user_likes.push({song: song.$key});
+    if (check == false) {
+      this.user_likes.push({ song: song.$key });
       this.songs.update(song.$key, { likes: song.likes + 1 });
+    }
   }
-}
   dislike(song) {
-    this.user_dislikes.push({song: song.$key});
+    this.user_dislikes.push({ song: song.$key });
     this.songs.update(song.$key, { likes: song.likes - 1 });
     //sleep(5);
   }
-}
-
-function sleep(seconds) {
-  var e = new Date().getTime() + seconds * 1000;
-  while (new Date().getTime() <= e) {}
 }
