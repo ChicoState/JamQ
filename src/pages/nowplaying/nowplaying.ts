9520b72e28ffa9705f8ed955c93fe3ab508ca4d2
logm <loganmarkle@gmail.com>
2017-11-28 10:38:29 -0800

    now shows hosts username at the top of the page

--- src/pages/nowplaying/nowplaying.ts
+++ src/pages/nowplaying/nowplaying.ts
@@ -3,11 +3,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { OAuth as OAuthWeb } from 'oauthio-web';
 import { OAuth } from 'oauth-phonegap';
 import { SpotifyWebApi } from 'spotify-web-api-node';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Platform } from 'ionic-angular';
 import { User } from '../../models/user';
+import {
+  AngularFireDatabase,
+  FirebaseListObservable,
+  FirebaseObjectObservable
+} from "angularfire2/database";
+import "rxjs/add/operator/map";
 
 
 @IonicPage()
@@ -16,23 +21,40 @@ import { User } from '../../models/user';
   templateUrl: 'nowplaying.html',
 })
 export class NowplayingPage {
-songs: FirebaseListObservable<any>;
-users: any;
-user = {} as User;
-key: any;
-isMobile: any;
-spotifyApi: any;
-partyKey: any;
+  songs: FirebaseListObservable<any>;
+  owner: FirebaseObjectObservable<any>;
+  users: any;
+  user = {} as User;
+  key: any;
+  isMobile: any;
+  spotifyApi: any;
+  partyKey: any;
+  host: any;
+  username: any;
 
 
-  constructor(public platform: Platform, public navCtrl: NavController, public navParams: NavParams,
-    public af: AngularFireDatabase, public afAuth: AngularFireAuth) {
+
+
+  constructor(
+    public platform: Platform,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public af: AngularFireDatabase,
+    public afAuth: AngularFireAuth
+  ) {
     if (platform.is('cordova')) { this.isMobile = true; }
-      else { this.isMobile = false; }
+    else { this.isMobile = false; }
 
+    this.afAuth.authState.subscribe(auth => {
+      this.af.object(`users/${auth.uid}`).take(1).subscribe(data => {
+        this.username = data.username;
+      });
+    });
 
     this.partyKey = sessionStorage['partyCookie'];
-    this.songs = af.list("/" + this.partyKey + "/songlist", {query: { limitToLast: 10 }});
+    this.songs = af.list("/" + this.partyKey + "/songlist", { query: { limitToLast: 10 } });
+    this.owner = af.object("/" + this.partyKey);
+    this.owner.subscribe(snapshot => (this.host = snapshot.owner));
     //this.songs = af.list("/333/songlist", { query: { limitToLast: 10 } });
 
     //This shows the party number at the top
@@ -43,50 +65,50 @@ partyKey: any;
     //build api with no params
     // this.spotifyApi = new SpotifyWebApi();
     // if(this.isMobile) {
-      //gets auth from cache named 'spotify'
+    //gets auth from cache named 'spotify'
     //   var spotify = OAuth.create('spotify');
     // } else {
-      //gets auth from cache named 'spotify' is Web
-      // var spotify = OAuthWeb.create('spotify');
+    //gets auth from cache named 'spotify' is Web
+    // var spotify = OAuthWeb.create('spotify');
     // }
     //sets access token of authenticated user
     // this.spotifyApi.setAccessToken(spotify.access_token);
 
-      // gets user data from api and asynchronously throws it into page
-      // checks if user has display name if not uses user id
-      // this.spotifyApi.getMe().then(function(data) {
-      //   if (data.body.display_name){
-      //     document.getElementById("name").innerHTML = data.body.display_name + "'s party";
-      //   } else {
-      //     document.getElementById("name").innerHTML = data.body.id + "'s party";
-      //   }
-      // }, function(err) { //error checking
-      //   console.log('Something went wrong!', err);
-      // })
+    // gets user data from api and asynchronously throws it into page
+    // checks if user has display name if not uses user id
+    // this.spotifyApi.getMe().then(function(data) {
+    //   if (data.body.display_name){
+    //     document.getElementById("name").innerHTML = data.body.display_name + "'s party";
+    //   } else {
+    //     document.getElementById("name").innerHTML = data.body.id + "'s party";
+    //   }
+    // }, function(err) { //error checking
+    //   console.log('Something went wrong!', err);
+    // })
 
 
-   //signs into firebase using anonymous user
-  //  this.afAuth.auth.signInAnonymously().catch(function(error) {
-  //    var errorMessage = error.message;
-  //  });
-}
+    //signs into firebase using anonymous user
+    //  this.afAuth.auth.signInAnonymously().catch(function(error) {
+    //    var errorMessage = error.message;
+    //  });
+  }
 
   remove(songid) {
     this.songs.forEach(song => {
       song.forEach(song => {
-      if(song.songid == songid) {
-        this.songs.remove(song.$key);
-      }
+        if (song.songid == songid) {
+          this.songs.remove(song.$key);
+        }
       })
     });
   }
 
   ionViewDidLoad() {
     try {
-       this.afAuth.authState.subscribe((auth) => {
+      this.afAuth.authState.subscribe((auth) => {
         //  console.log(auth)
-       });
-    } catch (e) {}
+      });
+    } catch (e) { }
     console.log('ionViewDidLoad NowplayingPage');
   }
 
